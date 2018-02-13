4011880faf046a6bd3dd2b6245e11345dc1c93e7
Joe Querin <7998323+joecue@users.noreply.github.com>
2018-02-13 11:06:59 -0500

    Added active category for each term
    Allows editors to choose what category is the active category for the specified term.
    Avoids having to rely on dates and determine correct year for the term.

--- php/lccc-academic-site-options.php
+++ php/lccc-academic-site-options.php
@@ -27,22 +27,40 @@ class new_lccc_academic_calendar_date_settings {
  function lccc_register_fields() {
   register_setting( 'general', 'lccc_spring_semester_startdate', 'esc_attr' );
 		register_setting( 'general', 'lccc_spring_semester_enddate', 'esc_attr' );
+		register_setting( 'general', 'lccc_spring_active_category', 'esc_attr' );
+		
 		register_setting( 'general', 'lccc_summer_semester_startdate', 'esc_attr' );
 		register_setting( 'general', 'lccc_summer_semester_enddate', 'esc_attr' );
+		register_setting( 'general', 'lccc_summer_active_category', 'esc_attr' );
+		
 		register_setting( 'general', 'lccc_fall_semester_startdate', 'esc_attr' );
 		register_setting( 'general', 'lccc_fall_semester_enddate', 'esc_attr' );
+		register_setting( 'general', 'lccc_fall_active_category', 'esc_attr' );		
+	
   add_settings_section( 'lccc-settings', 'LCCC Academic Calendar Settings', '__return_false', 'general' );
-  add_settings_field( 'lccc_spring_semester_startdate', '<label for="lccc_spring_semester_startdate">'.__('Spring Semester Start Date:' , 'lccc_spring_semester_startdate').'</label>', array(&$this, 'lccc_spring_semster_fields_html') , 'general', 'lccc-settings' );
+  
+		add_settings_field( 'lccc_spring_semester_startdate', '<label for="lccc_spring_semester_startdate">'.__('Spring Semester Start Date:' , 'lccc_spring_semester_startdate').'</label>', array(&$this, 'lccc_spring_semster_fields_html') , 'general', 'lccc-settings' );
 		
-  add_settings_field( 'lccc_spring_semester_enddate', '<label for="lccc_spring_semester_enddate">'.__('Spring Semester End Date:' , 'lccc_spring_semester_enddate').'</label>', array(&$this, 'lccc_spring_semster_end_fields_html') , 'general', 'lccc-settings' );
+		add_settings_field( 'lccc_spring_semester_enddate', '<label for="lccc_spring_semester_enddate">'.__('Spring Semester End Date:' , 'lccc_spring_semester_enddate').'</label>', array(&$this, 'lccc_spring_semster_end_fields_html') , 'general', 'lccc-settings' );
+		
+		add_settings_field( 'lccc_spring_active_category', '<label for="lccc_spring_active_category">'.__('Spring Active Category:', 'lccc_spring_active_category').'</label>', array(&$this, 'lccc_spring_active_category_fields_html') , 'general', 'lccc-settings' );
+		
 		add_settings_field( 'lccc_summer_semester_startdate', '<label for="lccc_summer_semester_startdate">'.__('Summer Semester Start Date:' , 'lccc_summer_semester_startdate').'</label>', array(&$this, 'lccc_summer_semster_fields_html') , 'general', 'lccc-settings' ); 
-			add_settings_field( 'lccc_summer_semester_enddate', '<label for="lccc_summer_semester_enddate">'.__('Summer Semester End Date:' , 'lccc_summer_semester_enddate').'</label>', array(&$this, 'lccc_summer_semster_end_fields_html') , 'general', 'lccc-settings' ); 
+		
+		add_settings_field( 'lccc_summer_semester_enddate', '<label for="lccc_summer_semester_enddate">'.__('Summer Semester End Date:' , 'lccc_summer_semester_enddate').'</label>', array(&$this, 'lccc_summer_semster_end_fields_html') , 'general', 'lccc-settings' ); 
+		
+		add_settings_field( 'lccc_summer_active_category', '<label for="lccc_summer_active_category">'.__('Summer Active Category:', 'lccc_summer_active_category').'</label>', array(&$this, 'lccc_summer_active_category_fields_html') , 'general', 'lccc-settings' );
+		
 		add_settings_field( 'lccc_fall_semester_startdate', '<label for="lccc_fall_semester_startdate">'.__('Fall Semester Start Date:' , 'lccc_fall_semester_startdate').'</label>', array(&$this, 'lccc_fall_semster_fields_html') , 'general', 'lccc-settings' );
+		
 		add_settings_field( 'lccc_fall_semester_enddate', '<label for="lccc_fall_semester_enddate">'.__('Fall Semester End Date:' , 'lccc_fall_semester_enddate').'</label>', array(&$this, 'lccc_fall_semster_end_fields_html') , 'general', 'lccc-settings' );
+		
+		add_settings_field( 'lccc_fall_active_category', '<label for="lccc_fall_active_category">'.__('Fall Active Category:', 'lccc_fall_active_category').'</label>', array(&$this, 'lccc_fall_active_category_fields_html') , 'general', 'lccc-settings' );
 		
  }
  
  function lccc_spring_semster_fields_html() {
+
 ?>
 <script>
 jQuery(document).ready(function(){
@@ -73,6 +91,23 @@ jQuery('#lccc_spring_semester_enddate').datepicker({
   echo '<p class="description" id="tagline-description">Enter the end date of the spring semester.</p>';
   echo '<p class="description" id="tagline-description"><strong>Example: May 12, 2016</strong></p>';
  }
+	
+function lccc_spring_active_category_fields_html() {
+		$lc_categories = get_categories( array(
+			'taxonomy' => 'event_categories',
+			'orderby' => 'name',
+			'order' => 'ASC',
+		) );
+	
+	$selectedCategory = get_option('lccc_spring_active_category', '' );
+
+  echo '<select name="lccc_spring_active_category" id="lccc_spring_active_category">';
+	foreach($lc_categories as $category){
+			echo '<option value="' . $category->slug .'" id="' . $category->slug . '"', $selectedCategory == $category->slug ? 'selected="selected"' : '', '>', $category->name, '</option>';
+			}
+		echo '</select>';
+	
+}
 function lccc_summer_semster_fields_html() {
 ?>
 <script>
@@ -103,6 +138,24 @@ jQuery('#lccc_summer_semester_enddate').datepicker({
   echo '<p class="description" id="tagline-description">Enter the end date of the summer semester.</p>';
   echo '<p class="description" id="tagline-description"><strong>Example: July 31, 2016</strong></p>';
  }
+	
+	function lccc_summer_active_category_fields_html() {
+		$lc_categories = get_categories( array(
+			'taxonomy' => 'event_categories',
+			'orderby' => 'name',
+			'order' => 'ASC',
+		) );
+	
+	$selectedCategory = get_option('lccc_summer_active_category', '' );
+
+  echo '<select name="lccc_summer_active_category" id="lccc_summer_active_category">';
+	foreach($lc_categories as $category){
+			echo '<option value="' . $category->slug .'" id="' . $category->slug . '"', $selectedCategory == $category->slug ? 'selected="selected"' : '', '>', $category->name, '</option>';
+			}
+		echo '</select>';
+	
+}
+	
 	function lccc_fall_semster_fields_html() {
 ?>
 <script>
@@ -134,5 +187,22 @@ jQuery('#lccc_fall_semester_enddate').datepicker({
   echo '<p class="description" id="tagline-description"><strong>Example: December 11, 2016</strong></p>';
  }
 
+	function lccc_fall_active_category_fields_html() {
+		$lc_categories = get_categories( array(
+			'taxonomy' => 'event_categories',
+			'orderby' => 'name',
+			'order' => 'ASC',
+		) );
+	
+	$selectedCategory = get_option('lccc_fall_active_category', '' );
+
+  echo '<select name="lccc_fall_active_category" id="lccc_fall_active_category">';
+	foreach($lc_categories as $category){
+			echo '<option value="' . $category->slug .'" id="' . $category->slug . '"', $selectedCategory == $category->slug ? 'selected="selected"' : '', '>', $category->name, '</option>';
+			}
+		echo '</select>';
+	
+}
+	
 }
 ?>
